9051dcb2ee80bd7c52dfd9f99dc06de1cde57757
Vidhi Gupta <Vidhi Gupta>
2016-03-09 16:42:13 +0530

    hashing algo modified to include full year in timestamp

--- hashing_algo.py
+++ hashing_algo.py
@@ -4,14 +4,14 @@ def rev(s):
     return s[::-1]
 
 def idToHash(n):
-    smap = "ABCDEF0123456789"
+    smap = "ABCD*#0123456789"
     ###(E: * , F: #)
     shorturl = ""
     while(n!=0):
-        a = int(n%128)
+        a = int(n%256)
         a = int(a%16)
         shorturl = shorturl + smap[a]
-        n = int(n/128)
+        n = int(n/256)
     shorturl = rev(shorturl)
     return shorturl
 
@@ -19,16 +19,16 @@ def hashToId(surl):
     ts = 0
     for i in surl:
         if("A"<=i and i<="F"):
-            ts = ts*128 + ord(i) - ord('A')
+            ts = ts*256 + ord(i) - ord('A')
         if("0"<=i and i<="9"):
-            ts = ts*128 + ord(i) - ord('0') + 6;
+            ts = ts*256 + ord(i) - ord('0') + 6;
     return ts
 
 nowt = str(datetime.datetime.now())
 print(nowt)
-i = 2
+i = 0
 x = ""
-while len(x)<12:
+while len(x)<14:
     if(nowt[i]>="0" and nowt[i]<="9"):
         x=x+nowt[i];
     i=i+1
